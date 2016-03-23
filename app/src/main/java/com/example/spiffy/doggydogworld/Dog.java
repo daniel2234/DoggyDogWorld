61c3d59f6104fe1783c9901fdb6f0cd0b337dc5d
Daniel <daniel2234@me.com>
2016-03-23 13:12:45 -0400

    Initial commit

---
+++ app/src/main/java/com/example/spiffy/doggydogworld/Dog.java
@@ -0,0 +1,18 @@
+package com.example.spiffy.doggydogworld;
+
+import android.util.Log;
+
+/**
+ * Created by Spiffy on 3/23/2016.
+ */
+public class Dog {
+    String name;
+    int age;
+    int numberOfLegs = 4;
+    String breed;
+
+    void bark(){
+        String bark = name + " barked";
+        Log.d("Dog",bark);
+    }
+}
